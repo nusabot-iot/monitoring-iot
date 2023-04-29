0c0efbb0a205614372886e799b2ecd6c4c7ff59f
Lorenz Adam Damara <lorenzrenz@gmail.com>
2023-04-29 10:16:16 +0700

    Perbaikan tampilan app

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      Aplikasi Monitoring dan Data Logging IoT
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Hak Cipta &copy; 2023 <a href="https://nusabot.id">Nusabot.id</a>.</strong>
 </footer>
